7b867a48c9ea99cb236345a7da4c444dae2e45cb
Pavel Liavitski <pashalevy@gmail.com>
2023-05-08 09:33:57 +0200

    resetting progress

--- word-clone/src/components/Game/Game.js
+++ word-clone/src/components/Game/Game.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-import GuessInput from '../GuessInput';
-import GuessResults from '../GuessResults';
+
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -11,16 +10,11 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  const [guesses, setGuesses] = React.useState([]);
 
-  function handleSubmitGuess(tentativeGuess) {
-    setGuesses([...guesses, tentativeGuess]);
-  }
 
   return (
     <>
-      <GuessResults guesses={guesses}/>
-      <GuessInput handleSubmitGuess={handleSubmitGuess} />
+    <p>code here</p>
     </>
   );
 }
