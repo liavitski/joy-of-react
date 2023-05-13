4b3ad5ff5e25810443be8b891d80153d479b8b45
Pavel Liavitski <pashalevy@gmail.com>
2023-05-13 20:08:57 +0200

    Guess component addeded

--- word-clone/src/components/GuessResults/GuessResults.js
+++ word-clone/src/components/GuessResults/GuessResults.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
+import Guess from '../Guess';
+
 function GuessResults({ rowOfGuesses }) {
   return (
     <div className="guess-results">
-      {rowOfGuesses.map((option, index) => (
-        <p className="guess" key={index}>
-          {option}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={rowOfGuesses[num]} />
       ))}
     </div>
   );
