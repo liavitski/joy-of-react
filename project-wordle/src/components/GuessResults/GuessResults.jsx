892d122b8d355b432f3f3ee5f70b9baaf0955381
Pavel Liavitski <pavel@Pavels-MacBook-Air.local>
2025-02-27 19:41:05 +0100

    updated

--- project-wordle/src/components/GuessResults/GuessResults.jsx
+++ project-wordle/src/components/GuessResults/GuessResults.jsx
@@ -4,7 +4,6 @@ import { range } from '../../utils';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guesses }) {
-
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((index) => (
