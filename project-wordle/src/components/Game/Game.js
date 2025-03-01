69d585f2bf01b88056e192b0b6c8ea1664c39258
Pavel Liavitski <pavel@Pavels-MacBook-Air.local>
2025-03-01 12:54:23 +0100

    test feature

--- project-wordle/src/components/Game/Game.js
+++ project-wordle/src/components/Game/Game.js
@@ -11,6 +11,8 @@ import WonBanner from '../WonBanner/WonBanner';
 import LostBanner from '../LostBanner/LostBanner';
 import Keyboard from '../Keyboard/Keyboard';
 
+/* some test code */
+
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
