b8b2745b383477f3acef26105990260a3198f67f
Pavel Liavitski <pashalevy@gmail.com>
2025-04-27 14:37:13 +0200

    third-exersice done

--- project-blog/src/app/[postSlug]/page.js
+++ project-blog/src/app/[postSlug]/page.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { loadBlogPost } from '@/helpers/file-helpers';
+import { BLOG_TITLE } from '@/constants';
 
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
 
-async function BlogPost({ params }) {
-  const { postSlug } = await params;
+export async function generateMetadata({ params }) {
+  const { frontmatter } = await loadBlogPost(params.postSlug);
 
-  const { frontmatter, content } = await loadBlogPost(postSlug);
+  return {
+    title: `${frontmatter.title} • ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  };
+}
+
+async function BlogPost({ params }) {
+  const { frontmatter, content } = await loadBlogPost(
+    params.postSlug
+  );
 
   return (
     <article className={styles.wrapper}>
