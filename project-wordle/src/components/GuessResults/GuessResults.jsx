2fb1d6c643d89cf5eb88829a125fd1110f52056e
Pavel Liavitski <pashalevy@gmail.com>
2025-06-24 21:09:14 +0300

    exercise-3 done

--- project-wordle/src/components/GuessResults/GuessResults.jsx
+++ project-wordle/src/components/GuessResults/GuessResults.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Guess from '../Guess/Guess';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ results }) {
   return (
     <div className="guess-results">
-      {results.map(({ guess, id }) => (
-        <p className="guess" key={id}>
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={results[num]} />
       ))}
     </div>
   );
